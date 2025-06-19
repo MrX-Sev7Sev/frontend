b36e56b9d8dab0c5d01c7bc0cbac847431640e9b
kapiw0n <flatline11@mail.ru>
2025-06-20 01:14:06 +0500

    integrated API_URL

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const API_URL = "https://eventmaster-0w4v.onrender.com";
+
+fetch(`${API_URL}/api/data`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP ошибка! Статус: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error("Ошибка:", error));
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
